235eb0ae6e097f4b59611f44046fbcca41afd119
wchhuangya <wchhuangyawork@163.com>
2016-03-23 11:51:57 +0800

    Revert "处理菜单+子菜单的关系"
    This reverts commit fd276ae15944864bd2eb88bc90f573bf331c4647.

--- web_excise/js/modules/introduction.js
+++ web_excise/js/modules/introduction.js
@@ -1,16 +1,13 @@
 $(function() {
 	$(".menu div").hover(function (event) {
 		if (!$(this).hasClass("current")) {$(this).addClass("current");}
-		if ($(this).hasClass("submenu")) {$(this).find("ul").show();}
 	}, function (event) {
 		var $this = $(this);
 		$this.siblings("div").each(function(i){
 			if ($(this).hasClass("current")) {$this.removeClass("current");}
 		});
-		if ($(this).hasClass("submenu")) {$(this).find("ul").hide();}
 	});
 	$(".menu div").click(function (event) {
 		$(this).addClass("current").siblings("div").removeClass("current");
 	});
-	$(".submenu ul").hide();
 });
